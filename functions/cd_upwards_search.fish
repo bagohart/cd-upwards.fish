290854d1bd7a7e7cbed8fb9c4498623e182280f2
Bagohart <bagohart@gmx.de>
2022-08-15 21:39:08 +0200

    renamed text to s because it is equally unhelpful, but at least a bit shorter

--- functions/cd_upwards_search.fish
+++ functions/cd_upwards_search.fish
@@ -1,21 +1,21 @@
-function cd_upwards_search --description 'Switches to the lowest directory which is upwards of current directory and matches the argument' --argument text
+function cd_upwards_search --description 'Switches to the lowest directory which is upwards of current directory and matches the argument' --argument s
 	if test (count $argv) = 0
         echo "No arguments passed, do nothing."
         return 1
     end
 
-    if string match --quiet --entire --regex / $text
-        echo "Argument $text contains '/', abort."
+    if string match --quiet --entire --regex / $s
+        echo "Argument $s contains '/', abort."
         return 1
     end
     set new_dir (path dirname (pwd))
-    if not string match --quiet --entire $text $new_dir
-        echo "The path above ($new_dir) doesn't contain $text, do nothing."
+    if not string match --quiet --entire $s $new_dir
+        echo "The path above ($new_dir) doesn't contain $s, do nothing."
         return 1
     end
 
     # this can't fail because the last 2 checks guarantee that it will be found
-    while not string match --quiet --entire $text (path basename $new_dir)
+    while not string match --quiet --entire $s (path basename $new_dir)
         set new_dir (path dirname $new_dir)
     end
 
